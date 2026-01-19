8b58bb72542fb52fc79aed892457345ea31680f9
8t-truck <kwoncar3@gmail.com>
2026-01-19 16:52:35 +0900

    Add

---
+++ coding_test/FileName.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(void) {
+	cout << "test ÀÔ´Ï´Ù.";
+	return 0;
+}
---
+++ coding_test/queue.hpp
@@ -0,0 +1,43 @@
+///*
+//queue, base on linked list
+//*/
+//#include<iostream>
+//using namespace std;
+//struct Node {
+//	int value;
+//	Node* next;
+//	Node* pre;
+//};
+////struct Header {
+////	Node* head;
+////};
+////struct Tail {
+////	Node* tail;
+////};
+//class Queue {
+//private:
+//	int elem_count;
+//	Node* h;
+//	Node* t;
+//
+//public:
+//	Queue() {
+//		/*h.head = NULL;
+//		t.tail = NULL;*/
+//		h
+//		elem_count = 0;
+//	}
+//	~Queue() {
+//		//nothing
+//	}
+//	void push(int n);
+//	void pop();
+//	int front();
+//	bool empty();
+//};
+//void Queue::push(int n) {
+//	if (elem_count == 0) {
+//		//first push
+//		
+//	}
+//}
